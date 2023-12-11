e42872f6691b2aba92dca00de489471198ae693c
kkarthikraj03 <rajkarthik090903@gmail.com>
2023-12-12 00:23:30 +0530

    Final Commit

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ CREATE TABLE IF NOT EXISTS employee (
     age INT NOT NULL,
     address VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
-    department VARCHAR(255) NOT NULL,
+    department VARCHAR(255),
     designation VARCHAR(255) NOT NULL,
-    joining_date DATE NOT NULL,
+    joining_date DATE,
     experience INT NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
-    phone_no VARCHAR(20) NOT NULL
+    phone_no VARCHAR(20),
 );
 `;
 
